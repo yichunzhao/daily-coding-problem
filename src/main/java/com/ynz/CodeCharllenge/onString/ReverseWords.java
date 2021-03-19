50ef94471ea23e08be7d341794cb4e8cc22ea57d
yichunzhao <zhaoyichun@hotmail.com>
2021-03-20 00:01:23 +0100

    reverse words in a String

---
+++ src/main/java/com/ynz/CodeCharllenge/onString/ReverseWords.java
@@ -0,0 +1,25 @@
+package com.ynz.CodeCharllenge.onString;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * reverse words in String
+ */
+public class ReverseWords {
+
+    public static void main(String[] args) {
+        String str = "I love this game";
+        System.out.println(str);
+        String[] words = str.split("\\s");
+
+        List<String> listString = Arrays.asList(words);
+        Collections.reverse(listString);
+
+        String reversedWords = listString.stream().collect(Collectors.joining(" "));
+        System.out.println(reversedWords);
+    }
+
+}
