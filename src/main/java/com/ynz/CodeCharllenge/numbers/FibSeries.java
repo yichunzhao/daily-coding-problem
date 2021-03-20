067e30d3691fca11ee8ce64f2f09cffc86aa39f7
yichunzhao <zhaoyichun@hotmail.com>
2021-03-20 11:06:43 +0100

    updating comments

--- src/main/java/com/ynz/CodeCharllenge/numbers/FibSeries.java
+++ src/main/java/com/ynz/CodeCharllenge/numbers/FibSeries.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 1. Fibonacci series (solution)
+ * Fibonacci series (solution)
  * Write a simple Java program which will print Fibonacci series, e.g. 1 1 2 3 5 8 13 ... .
  * up to a given number. We prepare for cross questions like using iteration over recursion
  * and how to optimize the solution using caching and memoization.
  * <p>
  * <p>
  * f(n) = f(n-1)+f(n-2); f(0)=0, f(1)=1; n>1
+ * <p>
+ * I provided an optimised method to calculate Fib numbers
  */
 public class FibSeries {
+    private static final int NUM_OF_FIB = 30;
     private static final int f0 = 0;
     private static final int f1 = 1;
 
@@ -31,14 +34,17 @@ public class FibSeries {
             fibNumbers.addAll(Arrays.asList(fn_2, fn_1));
         }
 
+        //determine exist from the recursive.
+        if (fibNumbers.size() >= NUM_OF_FIB) return;
+
+        //memory the value of f(n-1)
         int tmp = fn_1;
         fn_1 += fn_2;
 
         fibNumbers.add(fn_1);
         fn_2 = tmp;
 
-        if (fn_1 > 1_000_000) return;
-
+        //invoking itself
         fibNumber(fn_1, fn_2);
     }
 
