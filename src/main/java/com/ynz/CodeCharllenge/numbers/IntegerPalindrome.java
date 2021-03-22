5c45c1e3d833a86f9459a0e7fd899514cf09ab63
yichunzhao <zhaoyichun@hotmail.com>
2021-03-22 11:51:41 +0100

    updating: using remainder op. to reverse a number

--- src/main/java/com/ynz/CodeCharllenge/numbers/IntegerPalindrome.java
+++ src/main/java/com/ynz/CodeCharllenge/numbers/IntegerPalindrome.java
@@ -15,9 +15,15 @@ public class IntegerPalindrome {
 
         int input1 = 2345;
         System.out.printf("is %d a Palindrome? %b \n", input1, isPalindrome(input1));
+
+        System.out.printf("using remainder operation to reverse a number. \n");
+
+        System.out.printf("is %d a Palindrome? %b \n", input, isPalindrome(input));
+        System.out.printf("is %d a Palindrome? %s \n", input1, isPalindromeAnother(input1));
     }
 
     private static boolean isPalindrome(int input) {
+        if (input < 0) throw new IllegalArgumentException("negative int is not allowed");
         //convert input integer into a string
         String integerString = String.valueOf(input);
         String reversed = reverse(integerString);
@@ -26,12 +32,35 @@ public class IntegerPalindrome {
     }
 
     private static String reverse(String str) {
+        if (str == null || str.isEmpty()) throw new IllegalArgumentException("requiring a non-blank string");
         Deque<Character> stack = new LinkedList<>();
         char[] chars = str.toCharArray();
         for (char c : chars) {
             stack.push(c);
         }
 
-        return stack.stream().map(c -> String.valueOf(c)).collect(Collectors.joining());
+        return stack.stream().map(String::valueOf).collect(Collectors.joining());
     }
+
+    private static int reverse(int input) {
+        if (input < 0) throw new IllegalArgumentException("negative int is not allowed");
+
+        int reversed = 0;
+        int copy = input;
+
+        do {
+            int remainder = copy % 10;
+            copy = copy / 10;
+            reversed = reversed * 10 + remainder;
+        } while (copy > 0);
+
+        return reversed;
+    }
+
+    private static boolean isPalindromeAnother(int input) {
+        if (input < 0) throw new IllegalArgumentException("negative int is not allowed");
+
+        return reverse(input) == input;
+    }
+
 }
